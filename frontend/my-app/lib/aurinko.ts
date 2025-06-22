b760f8174a7d258889f1df3d63013c184c324e46
himanshu1844 <2022uch1043@mnit.ac.in>
2025-06-22 16:55:10 +0530

    trial

--- frontend/my-app/lib/aurinko.ts
+++ frontend/my-app/lib/aurinko.ts
@@ -1,5 +1,6 @@
 "use server"
 import { auth} from '@clerk/nextjs/server';
+import axios from 'axios';
 export async function getaurinko(serviceType: 'Google' | 'Office365'): Promise<string> {
     const { userId } = await auth();
     if (!userId) {
@@ -18,4 +19,74 @@ export async function getaurinko(serviceType: 'Google' | 'Office365'): Promise<s
     )
     return `https://api.aurinko.io/v1/auth/authorize?${params.toString()}`;
 
+}
+
+export const getToken = async (code: string) => {
+    console.log("Code received:", code);
+  
+    try {
+        const response = await axios.post(`https://api.aurinko.io/v1/auth/token/${code}`,
+            {},
+            {
+                auth: {
+                    username: process.env.AURINKO_Client_ID as string,
+                    password: process.env.AURINKO_CLIENT_SECRET as string,
+                    
+                }
+            }
+        );
+
+        return response.data as {
+            accountId: number,
+           accessToken : string,
+            userId: string,
+            userSession: string
+        }
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error('Error fetching Aurinko token:', error.response?.data || error.response?.status);
+        } else {
+            console.error('Unexpected error fetching Aurinko token:', error);
+        }
+    }
+}
+// export async function getToken(code:string){
+//      try {
+//             const response = await axios.post(`https://api.aurinko.io/v1/auth/token/${code}`, {}, {
+//                 auth: {
+//                     username: process.env.AURINKO_CLIENT_ID as string,
+//                     password: process.env.AURINKO_CLIENT_SECRET as string,
+//                 }
+//             });
+    
+//              return response.data as {
+//                     accountId: number,
+//                     accessToken: string,
+//                     userId: string,
+//                     userSession: string
+//                 }
+    
+//         } catch (error: any) {
+//             console.error('Error fetching token:', error.response ? error.response.data : error.message);
+//             throw new Error('Failed to fetch token');
+//         }
+    
+// }
+export async function getAccount(token: string) {
+    try {
+           const response = await axios.get('https://api.aurinko.io/v1/account', {
+               headers: {
+                   'Authorization': `Bearer ${token}`
+               }
+           });
+           return response.data as{
+               
+                email: string,
+                name: string
+           };
+           
+    } catch (error:any) {
+           console.error('Error fetching account:', error.response ? error.response.data : error.message);
+           throw new Error('Failed to fetch account');
+    }
 }
